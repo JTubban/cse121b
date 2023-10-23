73d6ef55e93b04001955c61bb95a5201a8a0d5d2
jtubban <nakoa024@gmail.com>
2023-10-23 13:50:09 +0800

    modified

---
+++ recipefinder/scripts/scripts.js
@@ -0,0 +1,13 @@
+let url = "https://github.com/JTubban/cse121b/blob/main/recipefinder/jsonfile/recipes.json";
+// let url = "https://byui-cse.github.io/cse121b-ww-course/resources/temples.json";
+let recipeList = [];
+
+const getRecipes = async () => {
+    let response = await fetch(url);
+    if (response.ok) {
+      recipeList = await response.json();
+        console.log(response);
+    }
+  } 
+
+getRecipes();
